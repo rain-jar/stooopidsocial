c443c5c892a29ff0572e6a3ae56fff1d50923391
Raj <raj.nair@colorado.edu>
2026-01-03 02:24:56 -0500

    footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-1 border-t border-white/10">
       <div className="mx-auto w-full max-w-[1400px] px-6 py-8 text-sm text-white/50">
-            <p className="mb-6 text-center text-lg text-white/100">
+            <p className="mb-6 text-center text-lg text-white/90">
                 Be good. Be kind. Be Stooopid
             </p>
         <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between">
